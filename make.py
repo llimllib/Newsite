3899882e25f0646a53ddb90251c393dfcfa97b37
bill <bill.mill@gmail.com>
2012-02-19 21:48:03 -0500

    generating blog entries

--- make.py
+++ make.py
@@ -2,9 +2,13 @@
 
 import sys
 
-from os.path import join, isdir
+from os import mkdir, unlink
+from glob import glob
 from shutil import copy, rmtree
-from shutil import rmtree
+from codecs import open
+from os.path import join, isdir, isfile, basename
+
+from pystache import render
 
 from lib.utils import copy_tree
 
@@ -12,8 +16,50 @@ def clean():
     if isdir("build"):
         rmtree("build")
 
+    if isfile("build"):
+        unlink("build")
+
 def build():
-    copy_tree(join("template", "960css"), join("build", "960css"))
+    def t(dir_): return join("template", dir_)
+
+    mkdir("build")
+
+    for dir_ in ("css", "images"):
+        copy_tree(t(dir_), join("build", dir_))
+    
+    copy(t("index.html"), "build/")
+
+    blog_template = open(t("blog_entry.mustache")).read()
+
+    for f in glob("blog_entries/*.txt"):
+        lines = open(f, encoding="utf8").readlines()
+
+        #the first line is the title
+        title = lines.pop(0).strip()
+
+        #read metadata
+        meta = {}
+        while lines[0].startswith("#"):
+            k, v = lines.pop(0).strip("#\n").split(" ", 1)
+            meta[k] = v
+            
+        txt = "".join(lines)
+
+        ### TODO
+        datelink = ""
+
+        output = render(blog_template, {
+            "title": title,
+            "content": txt,
+            "datelink": datelink
+        })
+
+        output_filename = join("build", basename(f).replace("txt", "html"))
+
+        if "i18" in output_filename:
+            import pdb; pdb.set_trace()
+
+        open(output_filename, "w", "utf8").write(output)
 
 def install():
     pass
