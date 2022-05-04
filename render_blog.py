12ed6d83d07ec930e787a15da4b5c1d1d9e0929c
Bill Mill <bill@billmill.org>
2022-05-04 09:40:26 -0400

    Automate blog entries on home page

--- render_blog.py
+++ render_blog.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
-from codecs import open
 from glob import glob
-from lib.utils import parse_bloxsom
 from os.path import join, basename
+from time import strftime, struct_time
+from typing import List, Any, Dict, Tuple
+
+from lib.utils import parse_bloxsom
 from pystache import render
-from time import strftime
 
 
 def relative_url(f):
@@ -23,7 +24,7 @@ def render_posts(posts):
     ).read()
 
     for f in posts:
-        title, meta, time_tuple, txt = parse_bloxsom(open(f, encoding="utf8"))
+        title, _, time_tuple, txt = parse_bloxsom(open(f, encoding="utf8"))
 
         timestr = strftime("%b %d, %Y", time_tuple)
 
@@ -34,22 +35,26 @@ def render_posts(posts):
             blog_template, {"title": title, "content": txt, "datelink": datelink}
         )
 
-        open(outname(f), "w", "utf8").write(output)
+        open(outname(f), "w", encoding="utf8").write(output)
 
 
-def get_recent_entries(n):
+# time, title, content, url, metadata
+Entry = Tuple[struct_time, str, str, str, Dict[str, str]]
+
+
+def get_recent_entries(n: int) -> List[Entry]:
     """return the n most recent blog posts"""
     entries = []
     for f in glob("blog_entries/*.txt"):
         url = basename(f[:-4] + ".html")
         title, meta, time_tuple, txt = parse_bloxsom(open(f, encoding="utf8"))
-        entries.append((time_tuple, title, txt, url))
+        entries.append((time_tuple, title, txt, url, meta))
 
     # sort them by date descending
-    return list(reversed(sorted(entries)))
+    return list(reversed(sorted(entries)))[:n]
 
 
-def render_feed(template, timefmt):
+def render_feed(template: str, timefmt: str, entries: List[Entry]) -> str:
     data = {
         "base_url": "http://billmill.org/",
         "blog_title": "My Name Rhymes",
@@ -60,16 +65,14 @@ def render_feed(template, timefmt):
         "blog_entries": [],
     }
 
-    entries = get_recent_entries(5)
-
     data["last_updated"] = strftime(timefmt, entries[0][0])
 
     # create the template dict
-    for time, title, txt, url in entries[:5]:
+    for time, title, txt, url, _ in entries:
         data["blog_entries"].append(
             {
                 "title": title,
-                "link": "http://billmill.org/%s" % url,
+                "link": "https://billmill.org/%s" % url,
                 "time": strftime(timefmt, time),
                 "desc": txt,
                 "text": txt,
@@ -79,16 +82,32 @@ def render_feed(template, timefmt):
     return render(template, data)
 
 
-def render_feeds():
-    rss_template = open(join("template", "rss.mustache"), encoding="utf8").read()
-    output = render_feed(rss_template, "%a, %d %b %Y %H:%M:%S +0000")
+def render_index(entries: List[Any]) -> None:
+    index_template = open(join("template", "index.html"), encoding="utf8").read()
+    data = {"blog_entries": []}
+    for time, title, txt, url, _ in entries:
+        data["blog_entries"].append(
+            {
+                "title": title,
+                "link": "https://billmill.org/%s" % url,
+                "time": strftime("%m %d, %Y", time),
+                "desc": txt,
+                "text": txt,
+            }
+        )
+    open("build/index.html", "w", encoding="utf8").write(render(index_template, data))
 
-    open("build/Rss", "w", "utf8").write(output)
+
+def render_feeds(entries: List[Entry]) -> None:
+    rss_template = open(join("template", "rss.mustache"), encoding="utf8").read()
+    output = render_feed(rss_template, "%a, %d %b %Y %H:%M:%S +0000", entries)
+
+    open("build/Rss", "w", encoding="utf8").write(output)
 
     atom_template = open(join("template", "atom.mustache"), encoding="utf8").read()
-    output = render_feed(atom_template, "%Y-%m-%dT%H:%M:%SZ")
+    output = render_feed(atom_template, "%Y-%m-%dT%H:%M:%SZ", entries)
 
-    open("build/Atom", "w", "utf8").write(output)
+    open("build/Atom", "w", encoding="utf8").write(output)
 
 
 if __name__ == "__main__":
@@ -106,6 +125,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.feeds:
-        render_feeds()
+        render_feeds(get_recent_entries(10))
     else:
         render_posts(args.files)
+        render_index(get_recent_entries(10))
